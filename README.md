91128812766e60ac86fb359d50174e72036f3a0b
NxtPerfect <alakaxan@protonmail.com>
2025-06-27 10:18:28 +0200

    INITIAL COMMIT
    feat: added tailwind

---
+++ README.md
@@ -0,0 +1,7 @@
+# Twenty Fourty Eight
+
+2048 game recreated in vue
+
+# Start
+
+to play the game simply run `bun run dev`
---
+++ src/components/organisms/Board.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+        const board = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]];
+</script>
+<template>
+        <table>
+                <tr v-for="row in board">
+                        <td v-for="cell in row">
+                                {{cell}}
+                        </td>
+                </tr>
+                </table>
+</template>
