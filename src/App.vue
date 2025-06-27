155c795592e0178c5a4131d4dc0b396201d5f609
NxtPerfect <alakaxan@protonmail.com>
2025-06-27 12:25:05 +0200

    feat: added moveDown functionality
    feat: added score calculation

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import Board from './components/organisms/Board.vue'
+import {reactive, ref} from 'vue'
+const board = reactive([[2, 2, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [2, 0, 0, 0]]);
+const score = ref(0);
+
+function moveDown() {
+        for (let i = 0; i < board.length - 1; i++) {
+                for (let j = 0; j < board[i].length; j++) {
+                        if (board[i][j] === 0) continue;
+                        if (board[i+1][j] === 0) {
+                                board[i+1][j] = board[i][j];
+                                board[i][j] = 0;
+                                continue;
+                        }
+                        if (board[i][j] === board[i+1][j]) {
+                                board[i+1][j] += board[i][j];
+                                board[i][j] = 0;
+                                score.value += board[i+1][j];
+                                continue;
+                        }
+                }
+        }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <Board/>
+        <main>
+                <div>
+                        <span>HIGHSCORE {{score}}</span>
+                        <Board :board="board" @click="moveDown"/>
+                </div>
+        </main>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- src/components/organisms/Board.vue
+++ src/components/organisms/Board.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-        const board = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]];
+defineProps<{ board: Array<Array<Number>> }>()
 </script>
 <template>
-        <table>
-                <tr v-for="row in board">
-                        <td v-for="cell in row">
+        <table class="flex flex-col items-center">
+                <tr v-for="row in board" class="flex flex-row justify-between">
+                        <td v-for="cell in row" class="p-2">
                                 {{cell}}
                         </td>
                 </tr>
