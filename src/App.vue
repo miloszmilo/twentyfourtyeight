f254b266faa6c788cc3fff4a9ecde6d2a1492805
NxtPerfect <alakaxan@protonmail.com>
2025-07-01 09:49:35 +0200

    fix: down, left and right movement properly moves empty squares

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import HelloWorld from './components/HelloWorld.vue'
 import Board from './components/organisms/Board.vue'
 import {reactive, ref} from 'vue'
-const board = reactive([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]);
+const board = reactive([[2, 2, 2, 2], [0, 2, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]);
 const isGameOver = ref(false);
 const score = ref(0);
 
@@ -15,6 +15,8 @@ const vectors = new Map<Direction, Array<Number>>([
 
 type Direction = "UP" | "DOWN" | "LEFT" | "RIGHT";
 
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+
 function move(direction: Direction) : None {
         const vectors = new Map<Direction, Array<Number>>([
                 ["UP", moveUp],
@@ -24,7 +26,8 @@ function move(direction: Direction) : None {
         ]);
         const moveFunction = vectors.get(direction);
         moveFunction()
-        spawnNewTiles();
+        // Temporary Disable
+        // spawnNewTiles();
 }
 
 function moveUp() {
@@ -47,18 +50,28 @@ function moveUp() {
 }
 
 function moveDown() {
-        for (let i = 0; i < board.length - 1; i++) {
+        for (let i = board.length-1; i > 0; i--) {
                 for (let j = 0; j < board[i].length; j++) {
-                        if (board[i][j] === 0) continue;
-                        if (board[i+1][j] === 0) {
-                                board[i+1][j] = board[i][j];
-                                board[i][j] = 0;
+                        if (board[i][j] === 0 && board.slice(0, i).map((a) => {return a[j]}).some(e => e > 0)) {
+                        const indexToMove = board.slice(0, i).map((a) => {return a[j]}).findIndex(e => e > 0);
+                                board[i][j] = board[indexToMove][j];
+                                board[indexToMove][j] = 0;
                                 continue;
                         }
-                        if (board[i][j] === board[i+1][j]) {
-                                board[i+1][j] += board[i][j];
+                        if (board[i][j] === board[i-1][j]) {
+                                board[i-1][j] += board[i][j];
                                 board[i][j] = 0;
-                                score.value += board[i+1][j];
+                                score.value += board[i-1][j];
+                                // Move any other tile
+                                for (let k = i; k > 0; k--) {
+                                        if (board[k-1][j] === 0) continue;
+                                        if (board[k][j] === 0) {
+                                                board[k][j] = board[k-1][j];
+                                                board[k-1][j] = 0;
+                                                continue;
+                                        }
+                                }
+                                continue;
                                 continue;
                         }
                 }
@@ -67,17 +80,26 @@ function moveDown() {
 
 function moveLeft() {
         for (let i = 0; i < board.length; i++) {
-                for (let j = board[i].length - 1; j > 0; j--) {
-                        if (board[i][j] === 0) continue;
-                        if (board[i][j-1] === 0) {
-                                board[i][j-1] = board[i][j];
-                                board[i][j] = 0;
+                for (let j = 0; j < board[i].length; j++) {
+                        if (board[i][j] === 0 && board[i].slice(j, board[i].length).some(e => e > 0)) {
+                        const indexToMove = board[i].slice(j, board[i].length).findIndex(e => e > 0);
+                                board[i][j] = board[i][indexToMove];
+                                board[i][indexToMove] = 0;
                                 continue;
                         }
-                        if (board[i][j] === board[i][j-1]) {
-                                board[i][j-1] += board[i][j];
-                                board[i][j] = 0;
-                                score.value += board[i][j-1];
+                        if (board[i][j] === board[i][j+1]) {
+                                board[i][j] += board[i][j+1];
+                                board[i][j+1] = 0;
+                                score.value += board[i][j];
+                                // Move any other tile
+                                for (let k = j; k < board[i].length - 1; k++) {
+                                        if (board[i][k+1] === 0) continue;
+                                        if (board[i][k] === 0) {
+                                                board[i][k] = board[i][k+1];
+                                                board[i][k+1] = 0;
+                                                continue;
+                                        }
+                                }
                                 continue;
                         }
                 }
@@ -86,19 +108,27 @@ function moveLeft() {
 
 function moveRight() {
         for (let i = 0; i < board.length; i++) {
-                for (let j = 0; j < board[i].length; j++) {
-                        if (board[i][j] === 0) continue;
-                        if (board[i][j+1] === 0) {
-                                board[i][j+1] = board[i][j];
-                                board[i][j] = 0;
+                for (let j = board[i].length; j > 0; j--) {
+                        if (board[i][j] === 0 && board[i].slice(0, j).some(e => e > 0)) {
+                        const indexToMove = board[i].slice(0, j).findIndex(e => e > 0);
+                                board[i][j] = board[i][indexToMove];
+                                board[i][indexToMove] = 0;
                                 continue;
                         }
-                        if (board[i][j] === board[i][j+1]) {
-                                board[i][j+1] += board[i][j];
-                                board[i][j] = 0;
-                                score.value += board[i][j+1];
-                                continue;
+                        if (board[i][j] !== board[i][j-1]) continue;
+                        board[i][j] += board[i][j-1];
+                        board[i][j-1] = 0;
+                        score.value += board[i][j];
+                        // Move any other tile
+                        for (let k = j; k > 0; k--) {
+                                if (board[i][k-1] === 0) continue;
+                                if (board[i][k] === 0) {
+                                        board[i][k] = board[i][k-1];
+                                        board[i][k-1] = 0;
+                                        continue;
+                                }
                         }
+                        continue;
                 }
         }
 }
@@ -108,11 +138,11 @@ function spawnNewTiles() {
         // 10% for 4 value
         const chance = Math.random();
         let tile = 2;
-        let insertTile = false;
+        let isTileInserted = false;
         if (chance > 0.9)
                 tile = 4;
         let isLost = true;
-        while (insertTile) {
+        while (!isTileInserted) {
                 for (let i = 0; i < board.length - 1; i++) {
                         for (let j = 0; j < board[i].length; j++) {
                                 if (board[i][j] !== 0) continue;
@@ -120,7 +150,7 @@ function spawnNewTiles() {
                                 const shouldChangeTile = Math.round(Math.random());
                                 if (shouldChangeTile === 1)
                                         board[i][j] = tile;
-                                        insertTile = true;
+                                        isTileInserted = true;
                                         return;
                         }
                 }
@@ -156,7 +186,11 @@ function startGame() {
                         <span>HIGHSCORE {{score}}</span>
                         <span>GAME OVER {{isGameOver ? 'LOST' : 'PLAYING'}}</span>
                         <button @click="startGame">Start</button>
-                        <Board :board="board" @click="move('DOWN')"/>
+                        <Board :board="board"/>
+                        <button @click="move('UP')">UP</button>
+                        <button @click="move('DOWN')">DOWN</button>
+                        <button @click="move('LEFT')">LEFT</button>
+                        <button @click="move('RIGHT')">RIGHT</button>
                 </div>
         </main>
 </template>
