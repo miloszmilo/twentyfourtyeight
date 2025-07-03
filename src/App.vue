7e9c2ca2e2dc4757c04bfb6e75cb7391a5159fb8
NxtPerfect <alakaxan@protonmail.com>
2025-07-03 07:23:09 +0200

    fix: moving left/right up/down doesn't shift elements

--- src/App.vue
+++ src/App.vue
@@ -26,15 +26,17 @@ function move(direction: Direction) : None {
         ]);
         const moveFunction = vectors.get(direction);
         moveFunction()
-        spawnNewTiles();
+        // spawnNewTiles();
 }
 
 function moveUp() {
         for (let i = 0; i < board.length; i++) {
                 for (let j = 0; j < board[i].length; j++) {
-                        const otherTilesInRow = board.slice(i+1, board.length).map((a) => {return a[j]})
-                        if (board[i][j] === 0 && otherTilesInRow.some(e => e > 0)) {
-                        const indexToMove = otherTilesInRow.findIndex(e => e > 0) + i + 1;
+                        const otherTilesInCol = board
+                                .slice(i+1, board.length).map((a) => {return a[j]})
+                        if (board[i][j] === 0 && otherTilesInCol.some(e => e > 0)) {
+                                const indexToMove = otherTilesInCol
+                                        .findIndex(e => e > 0) + i + 1;
                                 board[i][j] = board[indexToMove][j];
                                 board[indexToMove][j] = 0;
                                 continue;
@@ -62,9 +64,13 @@ function moveUp() {
 function moveDown() {
         for (let i = board.length - 1; i >= 0; i--) {
                 for (let j = 0; j < board[i].length; j++) {
-                        const otherTilesInRow = board.slice(0, i).map((a) => {return a[j]})
-                        if (board[i][j] === 0 && otherTilesInRow.some(e => e > 0)) {
-                        const indexToMove = otherTilesInRow.findIndex(e => e > 0);
+                        const otherTilesInCol = board
+                                .slice(0, i).map((a) => {return a[j]})
+                        if (board[i][j] === 0 && otherTilesInCol.some(e => e > 0)) {
+                                const indexToMove = findLastIndex(
+                                        otherTilesInCol,
+                                        e => e > 0
+                                );
                                 board[i][j] = board[indexToMove][j];
                                 board[indexToMove][j] = 0;
                                 continue;
@@ -92,9 +98,11 @@ function moveDown() {
 function moveLeft() {
         for (let i = 0; i < board.length; i++) {
                 for (let j = 0; j < board[i].length - 1; j++) {
-                        const otherTilesInRow = board[i].slice(j+1, board[i].length)
+                        const otherTilesInRow = board[i]
+                                .slice(j+1, board[i].length)
                         if (board[i][j] === 0 && otherTilesInRow.some(e => e > 0)) {
-                        const indexToMove = otherTilesInRow.findIndex(e => e > 0) + j + 1;
+                                const indexToMove = otherTilesInRow
+                                        .findIndex(e => e > 0) + j + 1;
                                 board[i][j] = board[i][indexToMove];
                                 board[i][indexToMove] = 0;
                                 continue;
@@ -123,7 +131,10 @@ function moveRight() {
                 for (let j = board[i].length - 1; j > 0; j--) {
                         const otherTilesInRow = board[i].slice(0, j)
                         if (board[i][j] === 0 && otherTilesInRow.some(e => e > 0)) {
-                        const indexToMove = otherTilesInRow.findIndex(e => e > 0);
+                                const indexToMove = findLastIndex(
+                                        otherTilesInRow,
+                                        e => e > 0
+                                );
                                 board[i][j] = board[i][indexToMove];
                                 board[i][indexToMove] = 0;
                                 continue;
@@ -146,6 +157,24 @@ function moveRight() {
         }
 }
 
+/**
+* https://stackoverflow.com/questions/40929260/find-last-index-of-element-inside-array-by-certain-condition/53187807#53187807
+* Returns the index of the last element in the array where predicate is true, and -1
+* otherwise.
+* @param array The source array to search in
+* @param predicate find calls predicate once for each element of the array, in descending
+* order, until it finds one where predicate returns true. If such an element is found,
+* findLastIndex immediately returns that element index. Otherwise, findLastIndex returns -1.
+*/
+function findLastIndex<T>(array: Array<T>, predicate: (value: T, index: number, obj: T[]) => boolean): number {
+    let l = array.length;
+    while (l--) {
+        if (predicate(array[l], l, array))
+            return l;
+    }
+    return -1;
+}
+
 function spawnNewTiles() {
         // 90% for 2 value
         // 10% for 4 value
@@ -156,7 +185,7 @@ function spawnNewTiles() {
                 tile = 4;
         let isLost = true;
         while (!isTileInserted) {
-                for (let i = 0; i < board.length - 1; i++) {
+                for (let i = 0; i < board.length; i++) {
                         for (let j = 0; j < board[i].length; j++) {
                                 if (board[i][j] !== 0) continue;
                                 isLost = false;
